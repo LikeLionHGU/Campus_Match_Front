ae93b3d714b46e7646d5eb12164d0625d0a6831b
wookmin <alsdnr030226@naver.com>
2026-02-06 18:07:08 +0900

    style: registe page

--- src/pages/register_page/Register_page.js
+++ src/pages/register_page/Register_page.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Register_page.css";
-
+import mainLogo from "../../assets/mainLogo.png"
 export default function RegisterPage() {
   const [form, setForm] = useState({
     userId: "",
@@ -24,131 +24,118 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="register-page">
 
-      <main className="content">
-        <section className="card">
-          <h1 className="title">회원가입</h1>
+    <div className="container">
 
-          <form className="form" onSubmit={onSubmit}>
-            <div className="field">
-              <label className="label">
-                아이디<span className="req">*</span>
-              </label>
+      <div className="content">
+        <div className="title">
+          <img src={mainLogo} alt="mainLogo" className="title-logo" />
+          <span>회원가입</span>
+        </div>
+
+        <form className="form" onSubmit={onSubmit}>
+          <div className="field">
+            <label className="label">
+              아이디<span className="req">*</span>
+            </label>
+            <input
+              className="input"
+              name="userId"
+              value={form.userId}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="field">
+            <label className="label">
+              비밀번호<span className="req">*</span>
+            </label>
+            <input
+              className="input"
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="field">
+            <label className="label">
+              이름<span className="req">*</span>
+            </label>
+            <input
+              className="input"
+              name="name"
+              value={form.name}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="field">
+            <label className="label">
+              대학<span className="req">*</span>
+            </label>
+            <div className="row">
               <input
                 className="input"
-                name="userId"
-                value={form.userId}
+                name="university"
+                value={form.university}
                 onChange={onChange}
               />
+              <button className="btn" type="button">
+                찾기
+              </button>
             </div>
+          </div>
 
-            <div className="field">
-              <label className="label">
-                비밀번호<span className="req">*</span>
-              </label>
+          <div className="field">
+            <label className="label">
+              전화번호<span className="req">*</span>
+            </label>
+            <div className="phone">
               <input
-                className="input"
-                type="password"
-                name="password"
-                value={form.password}
+                className="input phone-input"
+                name="phone1"
+                value={form.phone1}
                 onChange={onChange}
+                maxLength={3}
               />
-            </div>
-
-            <div className="field">
-              <label className="label">
-                이름<span className="req">*</span>
-              </label>
+              <span className="dash"></span>
               <input
-                className="input"
-                name="name"
-                value={form.name}
+                className="input phone-input"
+                name="phone2"
+                value={form.phone2}
                 onChange={onChange}
+                maxLength={4}
               />
-            </div>
-
-            <div className="field">
-              <label className="label">
-                대학<span className="req">*</span>
-              </label>
-              <div className="row">
-                <input
-                  className="input"
-                  name="university"
-                  value={form.university}
-                  onChange={onChange}
-                />
-                <button className="btn" type="button">
-                  찾기
-                </button>
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">동아리</label>
-              <div className="row">
-                <input
-                  className="input"
-                  name="club"
-                  value={form.club}
-                  onChange={onChange}
-                />
-                <button className="btn" type="button">
-                  찾기
-                </button>
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">
-                전화번호<span className="req">*</span>
-              </label>
-              <div className="phone">
-                <input
-                  className="input phone-input"
-                  name="phone1"
-                  value={form.phone1}
-                  onChange={onChange}
-                  maxLength={3}
-                />
-                <span className="dash">-</span>
-                <input
-                  className="input phone-input"
-                  name="phone2"
-                  value={form.phone2}
-                  onChange={onChange}
-                  maxLength={4}
-                />
-                <span className="dash">-</span>
-                <input
-                  className="input phone-input"
-                  name="phone3"
-                  value={form.phone3}
-                  onChange={onChange}
-                  maxLength={4}
-                />
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">
-                Email<span className="req">*</span>
-              </label>
+              <span className="dash"></span>
               <input
-                className="input"
-                name="email"
-                value={form.email}
+                className="input phone-input"
+                name="phone3"
+                value={form.phone3}
                 onChange={onChange}
+                maxLength={4}
               />
             </div>
+          </div>
 
-            <button className="submit" type="submit">
-              회원가입
-            </button>
-          </form>
-        </section>
-      </main>
-    </div>
+          <div className="field">
+            <label className="label">
+              이메일<span className="req">*</span>
+            </label>
+            <input
+              className="input"
+              name="email"
+              value={form.email}
+              onChange={onChange}
+            />
+          </div>
+
+          <button className="submit" type="submit">
+            회원가입
+          </button>
+        </form>
+      </div>
+    </div >
   );
 }
