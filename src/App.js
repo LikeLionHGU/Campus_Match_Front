575a381be868d50261c0a6d25c4bab098a0b03a7
wookmin <alsdnr030226@naver.com>
2026-02-19 14:21:47 +0900

    delete: 리액트 스타터팩

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import LoginPage from "./pages/login_page/Login_page";
 import RegisterPage from "./pages/register_page/Register_page";
 import MyPage from "./pages/MyPage/MyPage";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import DetailGallery from "./pages/Dashboard/Dashboard_Pages/ClubGalleryDetailPage/ClubGalleryDetailPage";
-import DetailRecord from "./pages/Dashboard/Dashboard_Pages/MatchupHistory/MatchupHistoryPage";
-import DetailCalender from "./pages/Dashboard/Dashboard_Pages/ClubCalenderDetailPage/ClubCalenderDetailPage";
 import MatchupBoardPage from "./pages/Matchup_Board_Page/MatchupBoardPage";
 import ClubSearchBoardPage from "./pages/ClubSearchBoardPage/ClubSearchBoardPage";
+import DetailGallery from "./pages/Dashboard/Dashboard_Pages/ClubGalleryDetailPage/ClubGalleryDetailPage";
+import MatchupHistoryPage from "./pages/Dashboard/Dashboard_Pages/MatchupHistory/MatchupHistoryPage";
+import DetailCalender from "./pages/Dashboard/Dashboard_Pages/ClubCalenderDetailPage/ClubCalenderDetailPage";
 import UpcomingMatchupPage from "./pages/Dashboard/Dashboard_Pages/ClubMatchupDetailPage/UpcomingMatchupPage/UpcomingMatchupPage";
 import OngoingMatchupPage from "./pages/Dashboard/Dashboard_Pages/ClubMatchupDetailPage/OngoingMatchupPage/OngoingMatchupPage";
 import ReceiveMatchupPage from "./pages/Dashboard/Dashboard_Pages/ClubMatchupDetailPage/RecieveMatchupPage/ReceiveMatchupPage";
 import SendMatchupPage from "./pages/Dashboard/Dashboard_Pages/ClubMatchupDetailPage/SendMatchupPage/SendMatchupPage";
+import FinishMatchupPage from "./pages/FinishMatchup/FinishMatchupPage";
 
 function App() {
   return (
@@ -27,7 +28,7 @@ function App() {
         <Route path="/makeClub" element={<MakeClub />} />
         <Route path="/myPage" element={<MyPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/record" element={<DetailRecord />} />
+        <Route path="/dashboard/record" element={<MatchupHistoryPage />} />
         <Route path="/dashboard/upcoming-matchup" element={<UpcomingMatchupPage />} />
         <Route path="/dashboard/ongoing-matchup" element={<OngoingMatchupPage />} />
         <Route path="/dashboard/receive-matchup" element={<ReceiveMatchupPage />} />
@@ -36,6 +37,7 @@ function App() {
         <Route path="/dashboard/gallery" element={<DetailGallery />} />
         <Route path="/matchup-board" element={<MatchupBoardPage />} />
         <Route path="/club-board" element={<ClubSearchBoardPage />} />
+        <Route path="/finish-matchup" element={<FinishMatchupPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
